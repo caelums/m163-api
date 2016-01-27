f90dba9c4d974a1cbf3755b8c6db45bf2539fe22
Chen Cong <ccaelums@gmail.com>
2016-01-27 23:08:31 +0800

    add limit

--- server.js
+++ server.js
@@ -13,7 +13,8 @@ router.get('/search', function *(next) {
     let s;
     if(this.query.query)
     {
-        s = {query:this.query.query,type:1,offset:0,limit:1};
+        let {query,type=1,offset=0,limit=10} = this.query;
+        s = {query:query,type:type,offset:offset,limit:limit};
     }else{
         s = {query:'不为谁而作的歌',type:1,offset:0,limit:1};
     }
